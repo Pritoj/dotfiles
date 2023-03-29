94409327f0ceedb5045fea7e035ca49d22a7805f
Pritoj Singh <pritojs@gmail.com>
2023-03-29 21:38:52 +0200

    eslint_d code actions

--- nvim/lua/custom/configs/null-ls.lua
+++ nvim/lua/custom/configs/null-ls.lua
@@ -1,64 +1,65 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-  return
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
 
-  -- Typescript, Javascript, HTML, CSS
-  b.formatting.prettierd.with {
-    filetypes = {
-      "html",
-      "markdown",
-      "css",
-      "typescript",
-      "javascript",
-      "typescriptreact",
-      "json",
-      "typescript.tsx",
-    },
-  },
-  b.diagnostics.eslint_d,
-  b.code_actions.eslint_d,
+	-- Typescript, Javascript, HTML, CSS
+	b.formatting.prettierd.with({
+		filetypes = {
+			"html",
+			"markdown",
+			"css",
+			"typescript",
+			"javascript",
+			"typescriptreact",
+			"json",
+			"typescript.tsx",
+			"scss",
+		},
+	}),
+	b.diagnostics.eslint_d,
+	b.code_actions.eslint_d,
 
-  -- Python
-  b.formatting.black,
-  b.diagnostics.mypy,
-  b.diagnostics.flake8,
+	-- Python
+	b.formatting.black,
+	b.diagnostics.mypy,
+	b.diagnostics.flake8,
 
-  -- Lua
-  b.formatting.stylua,
-  b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+	-- Lua
+	b.formatting.stylua,
+	b.diagnostics.luacheck.with({ extra_args = { "--global vim" } }),
 
-  -- Shell
-  b.formatting.shfmt,
-  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 
-  -- Git actions like stage etc
-  b.code_actions.gitsigns,
+	-- Git actions like stage etc
+	b.code_actions.gitsigns,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format,
 }
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-  on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format { bufnr = bufnr }
-        end,
-      })
-    end
-  end,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
