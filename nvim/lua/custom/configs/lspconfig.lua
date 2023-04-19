847b3bafa6480ccd8975c42fc1b72d26c15f87a4
Pritoj Singh <pritojs@gmail.com>
2023-04-19 22:08:44 +0200

    Hide diagnostic message when float is open e.g. autocomplete

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -49,9 +49,37 @@ for _, lsp in ipairs(servers) do
 	})
 end
 
--- Show diagnostic messages on hover https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
+-- Function to check if a floating dialog exists and if not
+-- then check for diagnostics under the cursor
+-- https://neovim.discourse.group/t/how-to-show-diagnostics-on-hover/3830/2
+function OpenDiagnosticIfNoFloat()
+	for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+		if vim.api.nvim_win_get_config(winid).zindex then
+			return
+		end
+	end
+	-- Show diagnostic messages on hover https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
+	-- THIS IS FOR BUILTIN LSP
+	vim.diagnostic.open_float(0, {
+		scope = "cursor",
+		focusable = false,
+		close_events = {
+			"CursorMoved",
+			"CursorMovedI",
+			"BufHidden",
+			"InsertCharPre",
+			"WinLeave",
+		},
+	})
+end
+-- Show diagnostics under the cursor when holding position
 vim.o.updatetime = 100
-vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]])
+vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+	pattern = "*",
+	command = "lua OpenDiagnosticIfNoFloat()",
+	group = "lsp_diagnostics_hold",
+})
 
 --
 -- lspconfig.pyright.setup { blabla}
