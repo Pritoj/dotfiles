871a37edcd47ffe3a90504f54fdae6c65697ac0b
Pritoj Singh <pritojs@amazon.com>
2023-05-31 10:16:58 +0000

    Rename

--- nvim/lua/custom/configs/rust-tools.lua
+++ nvim/lua/custom/configs/rust-tools.lua
@@ -3,12 +3,12 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 
 rt.setup({
 	server = {
-		on_attach = function(_, bufnr)
+		on_attach = function(client, bufnr)
 			-- Hover actions
 			vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
 			-- Code action groups
 			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-			on_attach(_, bufnr)
+			on_attach(client, bufnr)
 		end,
 		settings = {
 			-- to enable rust-analyzer settings visit:
